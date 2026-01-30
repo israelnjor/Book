7f4ae1a86bff7afe2eb895a251e284684278f3df
Israelnjor <ibkiche@st.ug.edu.gh>
2026-01-30 15:04:27 +0000

    Implement Main class with a Book instance

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main() {
+    Book b1 = new Book(
+      "My First Java Book",
+      "By Israelnjor",
+      "1283829738",
+      220,
+      350.90,
+    );
