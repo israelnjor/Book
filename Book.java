799e310d3199a0c937745de066854e60cd1bd44b
Israelnjor <ibkiche@st.ug.edu.gh>
2026-01-30 14:59:17 +0000

    Add Book class with properties and methods
    Implemented Book class with properties, constructors, and methods.

---
+++ Book.java
@@ -0,0 +1,38 @@
+public class Book {
+  private String title;
+  private String author;
+  private String isbn;
+  private int pageCount;
+  private double price;
+
+//Constructors
+  public Book() {}   //Default Constructor
+  public Book(String title, String author, String isbn,int pageCount, double price) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.pageCount = pageCount;
+    this.price = price;
+    
+//Setters
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public void setAuthor(String author) {this.author = author;}
+  public void setIsbn(String isbn) {this.isbn = isbn;}
+  public void setPrice(int pageCount)  {this.pageCount = pageCount;}
+  public void setPrice(double price( {this.price = price}
+
+//GETTERS
+  public String getTitle() {return title;}
+  public String getAuthor() {return author;}
+  public String getIsbn() {return isbn;}
+  public int getPageCount() {return pageCount;}
+
+//FULL GETTER
+  public String getFullDetails() {
+    return "Title: " + title +
+      ", Author: " + author +
+      ", ISBN: " + isbn +
+      ", No. of Pages: " + pageCount +
+      ", Price (GhC): " + price;
